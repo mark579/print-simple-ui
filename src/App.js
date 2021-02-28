c37a7e60f5aab1d5afca58449bd4a09ee19f89bd
Mark Davenport <markadavenport@gmail.com>
2021-02-28 15:30:16 -0600

    Orders now working

--- src/App.js
+++ src/App.js
@@ -1,95 +1,102 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Drawer from '@material-ui/core/Drawer';
-import Divider from '@material-ui/core/Divider';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Paper from '@material-ui/core/Paper';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import PrinterPanel from './PrinterPanel';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import PrintIcon from '@material-ui/icons/Print';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Drawer from "@material-ui/core/Drawer";
+import Divider from "@material-ui/core/Divider";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import PrinterPanel from "./PrinterPanel";
+import EtsyOrders from "./EtsyOrders";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import PrintIcon from "@material-ui/icons/Print";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+import "./App.css";
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    background: 'rgb(245,245,245)',
-    padding: theme.spacing(3, 2),
+    background: "rgb(245,245,245)",
+    display: "flex",
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
   },
   appBarShift: {
-    width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
   },
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginRight: 36,
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
+    whiteSpace: "nowrap",
   },
-  drawerPaper: {
+  drawerOpen: {
     width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
-  drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+  drawerClose: {
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: "hidden",
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: -drawerWidth,
-  },
-  contentShift: {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
   },
 }));
 
 export default function App() {
   const menu = {
-    'printers': 1,
-    'orders': 2
-  }
+    printers: 1,
+    orders: 2,
+  };
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = useState(false);
   const [data, setData] = useState(null);
-  const [selectedMenu, setSelectedMenu] = useState(menu.printers)
+  const [selectedMenu, setSelectedMenu] = useState(menu.printers);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -99,40 +106,57 @@ export default function App() {
     setOpen(false);
   };
 
-  const getStatus = ()=> {
-    fetch('/api/status')
-      .then(response => response.json())
-      .then(data => setData(data));
+  const getStatus = () => {
+    fetch("/api/status")
+      .then((response) => response.json())
+      .then((data) => setData(data));
   };
 
-  const renderPrinterPanels = ()=> {
-    if (data == null){
-      return "Loading..."
+  const renderPrinterPanels = () => {
+    if (data == null) {
+      return "Loading...";
     }
     const printers = data.printers;
     const items = [];
-    for (var printer of printers){
-      items.push(<PrinterPanel printer={printer} ports={data.ports} key={printer.name}/>)
+    for (var printer of printers) {
+      items.push(
+        <PrinterPanel printer={printer} ports={data.ports} key={printer.name} />
+      );
     }
-    return items
-  }
+    return items;
+  };
 
-  useEffect(()=> {
-    const interval = setInterval(()=> getStatus(), 1500);
+  useEffect(() => {
+    const interval = setInterval(() => getStatus(), 1500);
     return () => {
       clearInterval(interval);
-    }
+    };
   });
 
-  useEffect(()=> {
+  useEffect(() => {
     handleDrawerClose();
-  }, [selectedMenu])
+  }, [selectedMenu]);
 
-  return(
-    <div >
-      <AppBar position="static" color="primary">
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        color="primary"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
         <Toolbar>
-          <IconButton edge="start" className={clsx(classes.menuButton, open && classes.hide)} onClick={handleDrawerOpen} color="inherit" aria-label="menu">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit">
@@ -141,44 +165,64 @@ export default function App() {
         </Toolbar>
       </AppBar>
       <Drawer
-        className={classes.drawer}
-        variant="persistent"
-        anchor="left"
-        open={open}
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
         classes={{
-          paper: classes.drawerPaper,
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
         }}
       >
-        <div className={classes.drawerHeader}>
-          <Typography variant="h6" color="inherit">
-            Menu
-          </Typography>
+        <div className={classes.toolbar}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
         <List>
-          <ListItem button key='Printers' onClick={() => {setSelectedMenu(menu.printers)}}>
-            <ListItemIcon><PrintIcon/></ListItemIcon>
-            <ListItemText primary='Printers' />
+          <ListItem
+            button
+            key="Printers"
+            onClick={() => {
+              setSelectedMenu(menu.printers);
+            }}
+          >
+            <ListItemIcon>
+              <PrintIcon />
+            </ListItemIcon>
+            <ListItemText primary="Printers" />
           </ListItem>
-          <ListItem button key='Orders' onClick={() => {setSelectedMenu(menu.orders)}}>
-            <ListItemIcon><ShoppingCartIcon/></ListItemIcon>
-            <ListItemText primary='Orders' />
+          <ListItem
+            button
+            key="Orders"
+            onClick={() => {
+              setSelectedMenu(menu.orders);
+            }}
+          >
+            <ListItemIcon>
+              <ShoppingCartIcon />
+            </ListItemIcon>
+            <ListItemText primary="Orders" />
           </ListItem>
         </List>
       </Drawer>
-      {selectedMenu == menu.printers && 
-        <Paper className={classes.root} elevation={3} >
-          {renderPrinterPanels()}
-        </Paper>
-      }
-      {selectedMenu == menu.orders && 
-        <Paper className={classes.root} elevation={3} >
-          Orders Here!
-        </Paper>
-      }
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        {selectedMenu == menu.printers && (
+          <Paper elevation={3}>{renderPrinterPanels()}</Paper>
+        )}
+        {selectedMenu == menu.orders && (
+          <Paper elevation={3}><EtsyOrders /></Paper>
+        )}
+      </main>
     </div>
   );
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
+    '/etsy',
+    createProxyMiddleware({
+      target: 'http://localhost:8081',
+      pathRewrite: {
+        '^/etsy/': '/', // remove base path
+      },
+    })
+  );
+  app.use(
     '/api',
     createProxyMiddleware({
       target: 'https://print.madsunshinecreations.com',
